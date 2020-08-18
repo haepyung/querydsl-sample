380084e85d7eb6f728616f8e386a7827abf51829
haepyung <haepyung88@gmail.com>
2020-08-18 11:37:50 +0900

    feature : querydsl-sample 코드 프로젝트 생성
    querydsl 학습을 위한 기반 프로젝트 생성-1

---
+++ src/main/java/me/study/querydslsample/QuerydslSampleApplication.java
@@ -0,0 +1,14 @@
+package me.study.querydslsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslSampleApplication.class, args);
+        System.out.println("QueryDsl START~!");
+    }
+
+}
---
+++ src/test/java/me/study/querydslsample/QuerydslSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.querydslsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
