af4aad46127b2bbab38622db7b72971ea581cb6a
haepyung <haepyung88@gmail.com>
2021-01-27 22:12:54 +0900

    feature: querydsl 학습
    - querydsl Querydsl4RepositorySupport

--- src/main/java/me/study/querydslsample/repository/MemberRepositoryImpl.java
+++ src/main/java/me/study/querydslsample/repository/MemberRepositoryImpl.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 
 import javax.persistence.EntityManager;
+import java.io.IOException;
 import java.util.List;
 
 import static me.study.querydslsample.damain.QMember.member;
@@ -101,32 +102,35 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
   @Override
   public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
     List<MemberTeamDto> content = queryFactory
-        .select(new QMemberTeamDto(
-            member.id.as("memberId"),
-            member.username,
-            member.age,
-            team.id.as("teamId"),
-            team.name.as("teamName")))
-        .from(member)
-        .leftJoin(member.team, team)
-        .where(
-            usernameEq(condition.getUsername()),
-            teamNameEq(condition.getTeamName()),
-            ageGoe(condition.getAgeGoe()),
-            ageLoe(condition.getAgeLoe())
-        )
-        .offset(pageable.getOffset())
-        .limit(pageable.getPageSize())
-        .fetch();
+            .select(new QMemberTeamDto(
+                    member.id.as("memberId"),
+                    member.username,
+                    member.age,
+                    team.id.as("teamId"),
+                    team.name.as("teamName")))
+            .from(member)
+            .leftJoin(member.team, team)
+            .where(
+                    usernameEq(condition.getUsername()),
+                    teamNameEq(condition.getTeamName()),
+                    ageGoe(condition.getAgeGoe()),
+                    ageLoe(condition.getAgeLoe())
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+    NullPointerException e = new NullPointerException();
+    IOException e2 = new IOException();
 
     JPAQuery<Member> countQuery = queryFactory.selectFrom(member)
-        .leftJoin(member.team, team)
-        .where(
-            usernameEq(condition.getUsername()),
-            teamNameEq(condition.getTeamName()),
-            ageGoe(condition.getAgeGoe()),
-            ageLoe(condition.getAgeLoe())
-        );
+            .leftJoin(member.team, team)
+            .where(
+                    usernameEq(condition.getUsername()),
+                    teamNameEq(condition.getTeamName()),
+                    ageGoe(condition.getAgeGoe()),
+                    ageLoe(condition.getAgeLoe())
+            );
 
     //stestststsete
     return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
